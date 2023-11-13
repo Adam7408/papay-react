d5f458ed5611013c978cd3bd30f435fb4d540cef
Adam <yusufabuali74@gmail.com>
2023-11-13 19:52:52 +0900

    feat: develop logic of footers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,75 +1,65 @@
-import React from 'react'; 
-import '../css/App.css'; 
-import { Box, Button, Container, Stack, Typography } from '@mui/material'; 
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"; 
-import { RestaurantPage } from './screens/RestaurantPage/index'; 
-import { CommunityPage } from './screens/CommunityPage/index'; 
-import { OrdersPage } from './screens/OrdersPage/index'; 
-import { MemberPage } from './screens/MemberPage/index'; 
-import { HelpPage } from './screens/HelpPage/index'; 
-import { LoginPage } from './screens/LoginPage/index'; 
-import { HomePage } from './screens/HomePage/index'; 
- 
-function App() { 
-  return ( 
-    <Router> 
-      <div> 
-        <nav> 
-          <ul> 
-            <li> 
-              <Link to="/restaurant">RestaurantPage</Link> 
-            </li> 
-            <li> 
-              <Link to="/community">communityPage</Link> 
-            </li> 
-            <li> 
-              <Link to="/orders">OrdersPage</Link> 
-            </li> 
-            <li> 
-              <Link to="/member-page">MemberPage</Link> 
-            </li> 
-            <li> 
-              <Link to="/help">HelpPage</Link> 
-            </li> 
-            <li> 
-              <Link to="/login">Login</Link> 
-            </li> 
-            <li> 
-              <Link to="/">HomePage</Link> 
-            </li> 
-          </ul> 
-        </nav> 
-        <Switch> 
-          <Route path="/restaurant"> 
-            <RestaurantPage /> 
-          </Route> 
-          <Route path="/community"> 
-            <CommunityPage /> 
-          </Route> 
-          <Route path="/orders"> 
-            <OrdersPage /> 
-          </Route> 
-          <Route path="/member-page"> 
-            <MemberPage /> 
-          </Route> 
-          <Route path="/help"> 
-            <HelpPage /> 
-          </Route> 
-          <Route path="/login"> 
-            <LoginPage /> 
-          </Route> 
-          <Route path="/"> 
-              <HomePage /> 
-          </Route> 
-        </Switch> 
-      </div> 
-    </Router> 
-  ); 
-} 
- 
-export default App; 
- 
- 
-function Home() { 
- return <h2>Home</h2> 
+import React, { useState } from "react";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { RestaurantPage } from "./screens/RestaurantPage";
+import { CommunityPage } from "./screens/CommunityPage";
+import { OrdersPage } from "./screens/OrdersPage";
+import { MemberPage } from "./screens/MemberPage";
+import { HelpPage } from "./screens/HelpPage";
+import { LoginPage } from "./screens/LoginPage";
+import { HomePage } from "./screens/HomePage";
+
+import { NavbarHome } from "./components/header";
+import { NavbarRestaurant } from "./components/header/restaurant";
+import { NavbarOthers } from "./components/header/others";
+import { Footer } from "./components/footer";
+
+import "../css/App.css";
+import "../css/navbar.css";
+import "../css/footer.css";
+
+// routring tizimi - App da quriladi
+function App() {
+    const [path, setPath] = useState();
+    const main_path = window.location.pathname;
+    return (
+        <Router>
+            {main_path == "/" ? (
+                <NavbarHome setPath={setPath} />
+            ) : main_path.includes("/restaurant") ? (
+                <NavbarRestaurant setPath={setPath} />
+            ) : (
+                <NavbarOthers setPath={setPath} />
+            )}
+
+            <Switch>
+                <Route path="/restaurant">
+                    <RestaurantPage />
+                </Route>
+                <Route path="/community">
+                    <CommunityPage />
+                </Route>
+                <Route path="/orders">
+                    <OrdersPage />
+                </Route>
+                <Route path="/member-page">
+                    <MemberPage />
+                </Route>
+                <Route path="/help">
+                    <HelpPage />
+                </Route>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route path="/">
+                    <HomePage />
+                </Route>
+            </Switch>
+
+            <Footer />
+        </Router>
+    );
 }
+
+export default App;
